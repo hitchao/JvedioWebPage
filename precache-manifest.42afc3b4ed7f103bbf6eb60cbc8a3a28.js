8c2c24c9ad233faae0453c4aaf0b41d5ccb1a31e
hitchao <secret>
2022-01-10 19:57:43 +0800

    jvedio-vue

---
+++ precache-manifest.42afc3b4ed7f103bbf6eb60cbc8a3a28.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "1460842c7ee84254aef3",
+    "url": "/css/app.d3231039.css"
+  },
+  {
+    "revision": "1460842c7ee84254aef3",
+    "url": "/js/app.e58babcc.js"
+  },
+  {
+    "revision": "3e7d7843e1f39cad70f7",
+    "url": "/css/chunk-vendors.0ba8f5b6.css"
+  },
+  {
+    "revision": "3e7d7843e1f39cad70f7",
+    "url": "/js/chunk-vendors.134427ab.js"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d87c1b99dd4eb8caa1126d8fa0c8cd47",
+    "url": "/img/sample2.d87c1b99.png"
+  },
+  {
+    "revision": "aa9ee97e5ce03d5fde8d7a2812be64e0",
+    "url": "/img/sample4.aa9ee97e.png"
+  },
+  {
+    "revision": "9ad4e19cb66eb6f5fa4e6f9b91389ef7",
+    "url": "/img/sample3.9ad4e19c.png"
+  },
+  {
+    "revision": "92ac741bdb15c1d3717a4bd67a0f3b90",
+    "url": "/img/Windows.92ac741b.png"
+  },
+  {
+    "revision": "554d4c2acc079b0443d241179ded4c87",
+    "url": "/img/logo.554d4c2a.png"
+  },
+  {
+    "revision": "80d379df74406b8c9b4638781f309a43",
+    "url": "/img/sample1.80d379df.png"
+  },
+  {
+    "revision": "6393e33690410bf386e43f18dd26e72b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "14da206e873841fbbf914309e20d928d",
+    "url": "/files/config.json"
+  },
+  {
+    "revision": "9ba0640d1ac9fb7d5cd197c0f383ab71",
+    "url": "/files/Jvedio4.6.1.1.zip"
+  }
+];
